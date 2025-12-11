50972ffd1264b6d77e61e5bd4df06fb93038ad44
HuaMouRen <2825338723@qq.com>
2025-12-11 20:40:31 +0800

    feat: 更新guide分类

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,4 +5,15 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     bundler: viteBundler(),
     theme: defaultTheme(),
+
+    lang: 'zh-CN',
+    title: 'PvzLauncher 文档',
+    description: '植物大战僵尸启动器官方文档',
+
+
+    theme: defaultTheme({
+        navbar: [
+            { text: '指南', link: '/guide/'},
+        ]
+    })
 })
---
+++ docs/guide/getting-start.md
@@ -0,0 +1,12 @@
+---
+lang: zh-CN
+title: 快速开始
+description: 指引快速使用程序
+nav: true
+sidebar: true
+order: 1
+---
+
+# 快速使用
+
+none
